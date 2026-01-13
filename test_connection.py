1d5b54aa95575ce442c79691875af48d68f957a5
Harriman <eharriman@cotality.com>
2026-01-13 12:50:33 -0500

    test connect api

---
+++ test_connection.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FGT_FQDN = os.getenv("SYM_FGT_SB_FQDN")
+FGT_PORT = os.getenv("SYM_FGT_SB_PORT")
+API_KEY = os.getenv("SYM_FGT_SB_KEY")
+
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+url = f"https://{FGT_FQDN}:{FGT_PORT}/api/v2/monitor/system/status"
+headers = {'Authorization': f'Bearer {API_KEY}'}
+
+
+print(f"Connecting to {FGT_FQDN}...")
+try:
+    response = requests.get(url, headers=headers, verify=False, timeout=5)
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Success! Firmware Version: {data['version']}")
+    else:
+        print(f"Failed: {response.status_code}")
+except Exception as e:
+    print(f"Error: {e}")
