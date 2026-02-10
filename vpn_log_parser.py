3f8b843efd8fd0e9b38e158661bf5d34610e5e60
Ethan Harriman <eharriman@elhnet.com>
2026-02-10 16:02:33 -0500

    parse text using regex key-value pair

--- vpn_log_parser.py
+++ vpn_log_parser.py
@@ -4,4 +4,21 @@ LOG_File = "test_log.txt"
 
 VALID_USER_DICT = {"marvin","martin", "jsmith", "bwayne"}
 
+def parse_log_line(line):
+    # regex
+    pattern = re.compile(r'([a-zA-Z0-9_]+)=(?:"([^"]+)"|(\S+))')
+
+    log_dict = {}
+
+    for match in pattern.finditer(line):
+        key = match.group(1)
+        uid = match.group(2) if match.group(2) is not None else match.group(3)
+        log_dict[key] = uid
+    
+    return log_dict
+
+
+
+def analyze_log():
+
 
